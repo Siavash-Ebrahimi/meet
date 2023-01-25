94e6de3c6118204f317b00a6b5b95ee6fb8df687
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2023-01-25 13:20:24 +0400

    Adjust manifest.js file and service-worker for PWA

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorker from './service-worker';
 import reportWebVitals from './reportWebVitals';
 
+// Meet App Monitoring Atatus
 import * as atatus from 'atatus-spa';
 atatus.config('a1766f16199a4b6aa965be4eec820753').install();
 
@@ -18,7 +20,10 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+serviceWorkerRegistration.register();
+
+// serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
